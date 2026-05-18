f2dd60e1952bbb9fd1279ee2028fdb079ceb7c7f
natan <natan10_vieira@outlook.com>
2026-05-18 18:27:40 -0400

    adicionando css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import './App.css'
 //componentes
 import Formulario from './componentes/Formulario'
 
--- src/componentes/Formulario.jsx
+++ src/componentes/Formulario.jsx
@@ -1,23 +1,23 @@
 import { useState } from "react"
+import "../styles/Formulario.css"
 
 const Formulario = ()=> {
 
     return (<>
-                <form>
-                    <label>
+                <form className="formulario">
+                    <label className="formulario-label-nome">
                         <span>nome</span>
                         <input type="text" name="nome"  placeholder="nome"/>
                     </label>
-                    <label>
+                    <label className="formulario-label-idade">
                         <span>idade</span>
                         <input type="number" name="idade" placeholder="idade" />
                     </label>
-                    <label>
+                    <label className="formulario-label-email">
                         <span>email</span>
                         <input type="email" name="email"  placeholder="email"/>
                     </label>
                 </form>
-
     </>)
 }
 
